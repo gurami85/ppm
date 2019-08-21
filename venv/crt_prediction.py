0b78e9503647f4f3c110e2cc7f6ba3bda6d1cc72
gurami85 <gurami17@naver.com>
2019-08-21 18:58:55 +0900

    c

--- venv/crt_prediction.py
+++ venv/crt_prediction.py
@@ -1,10 +1,14 @@
+import torch
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # read data from input file
 training_data = pd.read_csv("./data/training_data.csv")
 print(training_data)
-# do feature scaling
+# do the feature scaling
 scaler = StandardScaler()
 training_data = scaler.fit_transform(training_data)
 print(training_data)
+
+torch.manual_seed(1)    # for random number generations
+
